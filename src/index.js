3ac929bb6e2d0fb151f73ec0819d6b1a8f05d3fa
5Maurice5 <maldunated@fcpn.edu.bo>
2026-09-06 00:06:41 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is running",
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
