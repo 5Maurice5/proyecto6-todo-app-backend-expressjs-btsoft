bc2578121fd2c9d4beb1d72e90fd27931773987d
5Maurice5 <maldunated@fcpn.edu.bo>
2026-09-07 16:44:05 -0400

    BTE-3: Correcciones

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ const createUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-      return res.status(400).json({
+      return res.status(422).json({
         message: "Name, email and password are required",
       });
     }
@@ -38,11 +38,11 @@ const createUser = async (req, res) => {
 
     return res.status(201).json({
       message: "User created successfully",
-      user: {
+      user: userDecorator({
         id,
         name,
         email,
-      },
+      }),
     });
   } catch (error) {
     console.error(error);
